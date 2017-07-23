ac5181591cf7fda7969cddde76a5db13f7fdc418
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-07-22 23:52:09 -0300

    ajustes finais parte2

--- teste.rb
+++ teste.rb
@@ -1,5 +1,9 @@
 require 'highline/import'
 
+load 'product.rb'
+load 'order.rb'
+load 'cart.rb'
+
 #cli = HighLine.new
 answer = ask("What do you think?")
 say("You have answered: #{answer}")
@@ -17,3 +21,44 @@ cli.choose do |menu|
   menu.choices(:python, :perl) { cli.say("Not from around here, are you?") }
   menu.default = :ruby
 end
+
+
+=begin
+puts "#### Test 01 ####"
+
+puts "Create product 01"
+product01 = Product.new({title:"Product 01", price:10})
+
+puts "Create order 01"
+order01 = Order.new({product:product01, quantity:2})
+
+puts order01.to_s
+
+puts "create cart"
+cart = Cart.new
+
+puts "Add order 01 to cart"
+cart.add(order01)
+
+puts "--- Cart show ---"
+
+cart.show
+
+puts "Total $#{cart.total_price}"
+
+order01.add_quantity()
+
+puts "--- Cart show ---"
+
+cart.show
+
+puts "Total $#{cart.total_price}"
+
+order01.add_quantity(3)
+
+puts "--- Cart show ---"
+
+cart.show
+
+puts "Total $#{cart.total_price}"
+=end
